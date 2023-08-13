b00a91cd3a702511029a07678702c0d1e9ba509f
paobd <88463109+paobd@users.noreply.github.com>
2023-08-12 21:17:22 -0500

    codigoPersonaje
    Se agrego código para seleccionar personajes con 300 cambios que mas jajajajajaj

---
+++ Assets/Scripts/CharacterSelection.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class CharacterSelection : MonoBehaviour
+{
+    public GameObject[] characters;
+    public GameObject character;
+       
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(GameObject character in characters)
+        {
+            character.SetActive(false);
+        }
+    }
+
+        public void SelectCharacter (int characterIndex)//selección personajes
+    {
+        for(int i = 0; i < characters.Length;i++)
+        {
+            if (i == characterIndex)
+            {
+                characters[i].SetActive(true);
+            } else
+            {
+                characters[i].SetActive(false);
+            }
+        }
+        SceneManager.LoadScene("MainScene");
+        Debug.Log("ya entra a otra scena");
+
+    } 
+}
