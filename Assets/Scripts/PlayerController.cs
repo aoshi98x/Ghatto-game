6a7ad39a5367126884865bcc8e4d0c84b8a6fdd1
Miguel Capibara <miguelx98@hotmail.com>
2023-08-12 19:45:53 -0500

    FoudCounterOK
    To Do -> Attack

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,19 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Movement Variables")]
     Rigidbody catRigid;
 
     [Range(0, 15)]
-    [SerializeField]float speed;
+    [SerializeField] float speed;
 
     [SerializeField] bool onFloor, canJump;
+
+    [Space(10)]
+    [Header("Collectibles Zone")]
+    [SerializeField] TextMeshProUGUI foodCount;
+    [SerializeField] int foodAmount;
+
+
     // Start is called before the first frame update
     void Start()
     {
         catRigid = GetComponent<Rigidbody>();
+        foodCount = GameObject.Find("FoodCounter").GetComponent<TextMeshProUGUI>();
     }
 
     // Update is called once per frame
@@ -40,7 +50,7 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.CompareTag("Floor"))
+        if (collision.gameObject.CompareTag("Floor"))
         {
             onFloor = true;
         }
@@ -50,6 +60,15 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Floor"))
         {
             onFloor = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Food"))
+        {
+            foodAmount++;
+            foodCount.text = "Food: " + foodAmount;
         }
     }
 }
